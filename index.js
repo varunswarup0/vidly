766f333bb099248659b816177e482d302656ff7e
varun <varunswarup0@gmail.com>
2020-04-23 04:11:21 +0530

    input Validation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require("joi");
 const express = require("express");
 const app = express();
+
+app.use(express.json());
 
 const courses = [
   { id: 1, name: "course1" },
@@ -13,6 +16,27 @@ app.get("/", (req, res) => {
 
 app.get("/api/courses", (req, res) => {
   res.send(courses);
+});
+
+app.post("/api/courses", (req, res) => {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+
+  if (result.error) {
+    //400 Bad Request
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+  courses.push(course);
+  res.send(course);
 });
 
 app.get("/api/courses/:id", (req, res) => {
