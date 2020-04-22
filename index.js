ed0f9ff66d8713de310357b6c491ee622fcd1fe1
varun <varunswarup0@gmail.com>
2020-04-23 03:04:03 +0530

    ENVIRONMENT VARIABLES

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
 
-app.listen(3000, () => {
-  console.log("Listening On Port 3000...");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening On Port ${port}...`);
 });
