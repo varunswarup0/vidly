6236ccd8cd3547f51c3942c59df353f4b39dfe83
varun <varunswarup0@gmail.com>
2020-04-23 12:05:46 +0530

    npm i morgan

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const helmet = require("helmet");
 const Joi = require("joi");
 const logger = require("./logger");
 const authenticater = require("./authenticater");
@@ -7,6 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(helmet());
 
 app.use(logger);
 app.use(authenticater);
