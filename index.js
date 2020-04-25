fc604f26129a41876db5407508256bebac9be62e
varun <varunswarup0@gmail.com>
2020-04-25 08:54:22 +0530

    Debugging

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const debug = require("debug")("app:startup");
 const config = require("config");
 const morgan = require("morgan");
 const helmet = require("helmet");
@@ -19,7 +20,7 @@ console.log("Mail Password: " + config.get("mail.password"));
 
 if (app.get("env") === "development") {
   app.use(morgan("tiny"));
-  console.log("Morgan enabled...");
+  debug("Morgan enabled...");
 }
 
 app.use(logger);
