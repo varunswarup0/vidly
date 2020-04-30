98d3384765fd2f80c59ff3d65381c8dfcd3c5217
varun <varunswarup0@gmail.com>
2020-04-30 05:57:21 +0530

    Resstrucuting the customer.js

--- routes/genres.js
+++ routes/genres.js
@@ -3,18 +3,6 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-const Genre = mongoose.model(
-  'Genre',
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-  })
-);
-
 router.get('/', async (req, res) => {
   const genres = await Genre.find().sort('name');
   res.send(genres);
