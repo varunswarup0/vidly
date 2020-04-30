4a391ea7fc7b9b492619a9d9b941ee902f5434b3
varun <varunswarup0@gmail.com>
2020-04-30 05:43:02 +0530

    Build The Cutomers API

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const Joi = require('joi');
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+mongoose
+  .connect('mongodb://localhost/playground', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch((err) => console.error('Could not connect to MongoDB...', err));
 
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
